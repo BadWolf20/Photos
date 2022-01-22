305a7676bd1685068b0b0a1184a7cd8a1d864583
BadWolf20 <Roman.Pichugin20@gmail.com>
2022-01-22 19:11:20 +0700

    creation main screen

---
+++ Photos/MainViewController.swift
@@ -0,0 +1,231 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by Roman on 22.01.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - Views
+    lazy var collectionView: UICollectionView = {
+        let view = UICollectionView(frame: view.bounds, collectionViewLayout: createCompLayout())
+        view.backgroundColor = Colors.ColectionViewBackGround
+
+        view.register(FirstTypeCell.self, forCellWithReuseIdentifier: FirstTypeCell.reuseId)
+        view.register(SecondTypeCell.self, forCellWithReuseIdentifier: SecondTypeCell.reuseId)
+        view.register(HeaderCollectionReusableView.self,
+                      forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                      withReuseIdentifier: HeaderCollectionReusableView.reuseId)
+
+        view.delegate = self
+        view.dataSource = self
+
+        return view
+    }()
+
+    lazy var sectionHeader: NSCollectionLayoutBoundarySupplementaryItem = {
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                      heightDimension: .absolute(Metric.headerHeight))
+
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top
+        )
+
+        return header
+    }()
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = "Settings"
+
+        let add = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(headerButton))
+        let play = UIBarButtonItem(title: "Play", style: .plain, target: self, action: #selector(headerButton))
+
+        navigationItem.rightBarButtonItems = [add, play]
+
+        setupHierarchy()
+        setupLayout()
+
+    }
+
+//    override func viewWillAppear(_ animated: Bool) {
+//        //navigationController?.navigationBar.isHidden = true
+//    }
+
+    // MARK: - Settings
+    private func setupHierarchy() {
+        view.addSubview(collectionView)
+    }
+
+    private func setupLayout() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor,
+                                            constant: Metric.collectionViewTopIndent).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    }
+
+}
+
+// MARK: - Functions
+extension MainViewController {
+
+    // Button actions
+    @objc func headerButton() {
+        print("action")
+    }
+
+    // Functions
+    func createCompLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout{ (sectionindex, layoutEnviroment) -> NSCollectionLayoutSection? in
+            let section = sectionsList[sectionindex]
+
+            switch section.type {
+            case 1:
+                return self.createPhotoSectionType1()
+            case 2:
+                return self.createPhotoSectionType2()
+            case 3, 4:
+                return self.createPhotoSectionType3()
+            default:
+                return self.createPhotoSectionType2()
+            }
+        }
+        return layout
+    }
+
+}
+
+// MARK: - collectionViewLayout
+extension MainViewController {
+    func createPhotoSectionType1() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(130 + 10),
+                                              heightDimension: .fractionalHeight(130 + 10 + 35))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(1),
+                                               heightDimension: .estimated(1))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item, item])
+        //group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
+        section.boundarySupplementaryItems = [sectionHeader]
+
+        return section
+    }
+
+    func createPhotoSectionType2() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(130 + 10),
+                                              heightDimension: .fractionalHeight(130 + 10 + 35))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(1),
+                                               heightDimension: .estimated(1))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        //group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0)
+
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
+        section.boundarySupplementaryItems = [sectionHeader]
+
+        return section
+    }
+
+    func createPhotoSectionType3() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(view.frame.width),
+                                              heightDimension: .fractionalHeight(50))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(1),
+                                               heightDimension: .estimated(1))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        //group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0)
+
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .none
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        section.boundarySupplementaryItems = [sectionHeader]
+
+        return section
+    }
+}
+
+// MARK: - UICollectionViewDataSource, UICollectionViewDelegate
+extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sectionsList.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sectionsList[section].album.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        switch sectionsList[indexPath.section].type {
+        case 1, 2:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FirstTypeCell.reuseId, for: indexPath) as? FirstTypeCell
+            cell?.configure(with: sectionsList[indexPath.section].album[indexPath.row])
+            return cell!
+        case 3, 4:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SecondTypeCell.reuseId, for: indexPath) as? SecondTypeCell
+            cell?.configure(with: sectionsList[indexPath.section].album[indexPath.row])
+            return cell!
+        default:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FirstTypeCell.reuseId, for: indexPath) as? FirstTypeCell
+            cell?.configure(with: sectionsList[indexPath.section].album[indexPath.row])
+            return cell!
+        }
+
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //moveToDogSetup()
+        print(123)
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: HeaderCollectionReusableView.reuseId,
+            for: indexPath) as! HeaderCollectionReusableView
+        header.setTittle(title: sectionsList[indexPath.section].title)
+        return header
+    }
+
+
+
+}
+
+// MARK: - Constants
+
+extension MainViewController {
+    enum Colors {
+        static let ColectionViewBackGround: UIColor = .orange
+    }
+
+    enum Metric {
+        // SectionHeader
+        static let headerHeight: CGFloat = 30
+        // collectionView
+        static let collectionViewTopIndent: CGFloat = 0
+    }
+}
---
+++ Photos/PhotoData.swift
@@ -0,0 +1,110 @@
+//
+//  PhotoData.swift
+//  Photos
+//
+//  Created by Roman on 22.01.2022.
+//
+import Foundation
+import UIKit
+
+struct Album {
+    let title: String
+    let photos: [UIImage?]
+    let icon: UIImage?
+}
+
+struct section {
+    let type: Int
+    let title: String
+    let album: [Album]
+}
+
+let myAlbums: [Album] = [
+    Album(title: "Недавние",
+          photos: [UIImage(named: "Pavel-0")
+                  ],
+          icon: nil),
+    Album(title: "Избранное",
+          photos: [UIImage(named: "Danil-0"),
+                   UIImage(named: "Pavel-0")
+                  ],
+          icon: nil),
+    Album(title: "Instagram",
+          photos: [UIImage(named: "photo-1"),
+                   UIImage(named: "photo-2")
+                  ],
+          icon: nil),
+    Album(title: "Twitter",
+          photos: [UIImage(named: "photo-3"),
+                   UIImage(named: "photo-4")
+                  ],
+          icon: nil)
+]
+
+let peopleAndPlaces: [Album] = [
+    Album(title: "Люди",
+          photos: [UIImage(named: "photo-5"),
+                   UIImage(named: "photo-6")
+                  ],
+          icon: nil),
+    Album(title: "Места",
+          photos: [UIImage(named: "photo-7")
+                  ],
+          icon: nil),
+    Album(title: "Виды",
+          photos: [UIImage(named: "photo-8"),
+                   UIImage(named: "photo-9")
+                  ],
+          icon: nil),
+    Album(title: "Друзья",
+          photos: [UIImage(named: "photo-10"),
+                   UIImage(named: "photo-11")
+                  ],
+          icon: nil)
+]
+
+let mediaTypes: [Album] = [
+    Album(title: "Видео",
+          photos: [UIImage(named: "photo-12")
+                  ],
+          icon: UIImage(systemName: "video")),
+    Album(title: "Селфи",
+          photos: [UIImage(named: "photo-13"),
+                   UIImage(named: "photo-14"),
+                   UIImage(named: "photo-15")
+                  ],
+          icon: UIImage(systemName: "person.crop.square.fill")),
+    Album(title: "Фото Live Photos",
+          photos: [UIImage(named: "photo-16")
+                  ],
+          icon: UIImage(systemName: "livephoto")),
+    Album(title: "Портреты",
+          photos: [UIImage(named: "photo-17"),
+                   UIImage(named: "photo-18")
+                  ],
+          icon: UIImage(systemName: "cube"))
+]
+
+let other: [Album] = [
+    Album(title: "Импортированные",
+          photos: [UIImage(named: "photo-19")
+                  ],
+          icon: UIImage(systemName: "square.and.arrow.down")),
+    Album(title: "Скрытые",
+          photos: [UIImage(named: "photo-20"),
+                   UIImage(named: "photo-21")
+                  ],
+          icon: UIImage(systemName: "eye.slash")),
+    Album(title: "Недавно удаленные",
+          photos: [UIImage(named: "photo-22"),
+                   UIImage(named: "photo-23")
+                  ],
+          icon: UIImage(systemName: "trash"))
+]
+
+let sectionsList = [
+    section(type: 1, title: "Мои альбомы", album: myAlbums),
+    section(type: 2, title: "Люди и места", album: peopleAndPlaces),
+    section(type: 3, title: "Типы медиафайлов", album: mediaTypes),
+    section(type: 4, title: "Другое", album: other)
+]
